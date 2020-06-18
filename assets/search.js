f3f6b74c7e129242420ef14cc5175361d0d50944
Wanderingtech <heather86lyn@gmail.com>
2020-06-17 17:46:40 -0700

    Minimal work on API calls

--- assets/search.js
+++ assets/search.js
@@ -1,5 +1,5 @@
 //Access Civic API
-
+var userAddress= 
 var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?address=1717%20SW%20Park%20Drive%2C%20Apt%201224&includeOffices=true&key=AIzaSyDYsucFLhfwF4iEpT9CrAD7rCFdUvrQ87E";
 // var APIKey = "AIzaSyDYsucFLhfwF4iEpT9CrAD7rCFdUvrQ87E" 
 $.ajax({
@@ -12,5 +12,10 @@ $.ajax({
 
 
 //If local box checked #localCheck
+var localCheck= "/ocd-division\/country:us\/state:\D{2}\/place:\D+/;"
+//If County box checked #countyCheck
+var countyCheck= "/ocd-division\/country:us\/state:\D{2}\/county:\D+/;"
 //If State box checked #stateCheck
+var stateCheck= "/ocd-division\/country:us\/state:\D{2}\/county:\D+/;"
 //If Federal box checked #federalCheck
+var federalCheck= "ocd-division/country:us";
